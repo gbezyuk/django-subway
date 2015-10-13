212fe7d1212d62dd2a7342ccabe569d3b0c2ac51
Grigoriy Beziuk <gbezyuk@gmail.com>
2015-10-13 10:47:08 +0300

    cleanup

--- plain_frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url, patterns
-from .views import home, assignations, developers, about
-
-urlpatterns = (
-    url(r'^$', home, name='home'),
-    url(r'^assignations/', assignations, name='assignations'),
-    url(r'^developers/', developers, name='developers'),
-    url(r'^about/', about, name='about'),
-)
