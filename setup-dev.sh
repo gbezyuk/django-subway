8a9c6e93b855043df67f5edd7d79bd2632427c23
Grigoriy Beziuk <gbezyuk@gmail.com>
2015-10-13 11:05:58 +0300

    setup-dev.sh cleanup

--- setup-dev.sh
+++ setup-dev.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
-echo 'This script will setup Real Estate django project development environemnt for you\n'
+echo 'This script will setup Django Starter project development environemnt for you\n'
 
 echo 'Compiling translations; you need to have gettext installed'
-
 cd core
-../manage.py compilemessages -l ru
-cd ..
-
-cd immovables
-../manage.py compilemessages -l ru
-cd ..
-
-cd subway
 ../manage.py compilemessages -l ru
 cd ..
 echo '\n'
@@ -33,14 +24,8 @@ echo 'Initializing database structure'
 echo '\n'
 
 echo 'Installing fixtures'
-echo ' - subway'
-./manage.py loaddata subway/fixtures/spb.json
-echo ' - districts'
-./manage.py loaddata immovables/fixtures/districts.json
-echo ' - flat types'
-./manage.py loaddata immovables/fixtures/flat_types.json
-echo ' - finishing types'
-./manage.py loaddata immovables/fixtures/finishing_types.json
+#echo ' - subway'
+#./manage.py loaddata subway/fixtures/spb.json
 echo '\n'
 
 echo 'All done. you are ready to activate your virtual environment, create superuser and run the development server'
