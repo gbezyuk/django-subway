22680083928858a48abef06f47f24952d753a228
Vladimir A Filonov <i@vladimir.filonov.name>
2015-10-15 15:53:09 +0300

    Fix indent of order_with_respect_to in Station model

--- subway/models.py
+++ subway/models.py
@@ -13,15 +13,16 @@ class Branch(NameTimestampsModel, OrderedModel, IsEnabledModel):
     def get_absolute_url(self):
         return reverse('subway-branch-detail', kwargs={'id': self.id})
 
-    color = RGBColorField(colors=('#FF0000', '#0070BB', '#009245', '#FBA820', '#662C91'), verbose_name=_('color'))
+    color = RGBColorField(colors=('#FF0000', '#0070BB', '#009245', '#FBA820',
+                                  '#662C91'), verbose_name=_('color'))
 
 
 class Station(NameTimestampsModel, OrderedModel, IsEnabledModel):
+
     class Meta(OrderedModel.Meta):
         verbose_name = _('subway station')
         verbose_name_plural = _('subway stations')
-
-    order_with_respect_to = 'branch'
+        order_with_respect_to = 'branch'
 
     def get_absolute_url(self):
         return reverse('subway-station-detail', kwargs={'id': self.id})
